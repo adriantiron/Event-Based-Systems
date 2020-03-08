536fcf69aee883bbd53e8cc6f2571c75f6fd4132
adriantiron <tda_mda@yahoo.com>
2020-03-08 11:02:39 +0200

    Publications generator

---
+++ source.py
@@ -0,0 +1,14 @@
+import random as rd
+from var_limits import *
+
+pub_nb = 10
+with open("pub.txt", "a") as fd:
+    for x in range(0, pub_nb):
+        args = {'bank': rd.choice(banks_list), 'trans': rd.choice(trans_type), 'date': rd.choice(dates_list),
+                'amount': rd.randrange(amo_int['low'], amo_int['high']),
+                'balance': rd.randrange(bal_int['low'], bal_int['high'])}
+
+        publication = "{{(bank,{bank});(transaction,{trans});(amount,{amount});(balance,{balance});(date,{date})}}\n"\
+                      .format(**args)
+
+        fd.write(publication)

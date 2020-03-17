7eff5b453db4560fd754f6d08b1b9564ca4ffe1e
adriantiron <tda_mda@yahoo.com>
2020-03-17 11:29:20 +0200

    Update publications.py

--- publications.py
+++ publications.py
@@ -2,7 +2,7 @@ import random as rd
 from var_limits import *
 
 pub_nb = 10
-with open("pub.txt", "a") as fd:
+with open("res/pub.txt", "a") as fd:
     for x in range(0, pub_nb):
         args = {'bank': rd.choice(banks_list), 'trans': rd.choice(trans_type), 'date': rd.choice(dates_list),
                 'amount': rd.randrange(amo_int['low'], amo_int['high']),
